f9e2f5db19fbe6005efe88825b3077bd11a08b73
cat161 <ojamal@student.1337.ma>
2023-04-11 00:41:41 +0000

    added ft_printf

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+void ft_putchar(char c, int *len)
+{
+	write(1, &c, 1);
+	*len = *len + 1;
+}
+
+void	ft_putstr(char *str, int *len)
+{
+	int i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i], len);
+		i++;
+	}
+}
+
+void ft_putnbr(int n, int *len)
+{
+	if (n <= 9)
+	{
+		n += '0';
+		ft_putchar(n, len);
+	}
+	else
+	{
+		ft_putnbr(n / 10, len);
+		ft_putnbr(n % 10, len);
+	}
+}
+
+void ft_puthex(unsigned int n, int *len)
+{
+	if (n <= 16)
+	{
+		n += '0';
+		ft_putchar(n["0123456789abcdef"], len);
+		*len = *len + 1;
+	}
+	else
+	{
+		ft_puthex(n / 16, len);
+		ft_puthex(n % 16, len);
+	}
+}
+
+int ft_printf(const char *str, ...)
+{
+	va_list ap;
+	int len = 0;
+	int i = 0;
+	va_start(ap, str);
+	while(str[i])
+	{
+		if(str[i] == '%' && str[i + 1])
+		{
+			i++;
+			if (str[i] == 's')
+				ft_putstr(va_arg(ap, char *), &len);
+			else if (str[i] == 'd')
+				ft_putnbr(va_arg(ap, int), &len);
+			else if (str[i] == 'x')
+				ft_puthex(va_arg(ap, unsigned int), &len);
+		}
+		else
+			ft_putchar(str[i], &len);
+			i++;
+	}
+	va_end(ap);
+	return len;
+}
